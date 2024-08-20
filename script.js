817513a533b6cf3ee1294da384315331fc44fdc1
Ortiz-20 <gndriver00@gmail.com>
2024-08-21 02:51:06 +0700

    latihan js

---
+++ script.js
@@ -0,0 +1,29 @@
+let h1 = "Latihan JavaScript Selesai";
+let name = "john";
+const age = 30;
+let fruit=["apple", "banana","mango"];
+let person = {firstName: "John", lastname: "dodo", age: 30};
+//document.getElementById().innerHTML
+//document.getElementById().value
+
+
+
+let x = 10;
+let y = 5;
+
+if (x > 10) {
+    console.log("X lebih besar dari 10");
+} else {
+    console.log("X tidak lebih besar dari 10");
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log("Iterasi ke-" + i);
+}
+
+function greet(name) {
+    return "Hello, " + name;
+}
+let message = greet("John"); 
+
+function calculateSquare(number)
